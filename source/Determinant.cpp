aff2295006fae28512b429aa8e6c4ff56a8fa419
askar <agabitov25@gmail.com>
2022-05-21 14:34:21 +0300

    aa

---
+++ source/Determinant.cpp
@@ -0,0 +1,5 @@
+//
+// Created by askar on 21.05.22.
+//
+
+#include "Determinant.h"
---
+++ source/Determinant.h
@@ -0,0 +1,8 @@
+//
+// Created by askar on 21.05.22.
+//
+
+#ifndef CASUAL_DETERMINANT_H
+#define CASUAL_DETERMINANT_H
+
+#endif //CASUAL_DETERMINANT_H
